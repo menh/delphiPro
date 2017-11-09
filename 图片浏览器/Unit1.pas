a68d517afec66f80f59b948023be8c36964ed112
menh <872204778@qq.com>
2017-11-09 23:59:37 +0800

    11

--- 图片浏览器/Unit1.pas
+++ 图片浏览器/Unit1.pas
@@ -4,17 +4,20 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, frxpngimage, ExtCtrls, FileCtrl;
+  Dialogs, StdCtrls,  ExtCtrls, FileCtrl;
 
 type
   TForm1 = class(TForm)
-    Button1: TButton;
     Panel1: TPanel;
     Panel2: TPanel;
     DriveComboBox1: TDriveComboBox;
     DirectoryListBox1: TDirectoryListBox;
     Panel3: TPanel;
     FilterComboBox1: TFilterComboBox;
+    CheckBoxStretch: TCheckBox;
+    ButtonFull: TButton;
+    FileListBox1: TFileListBox;
+    Image1: TImage;
   private
     { Private declarations }
   public
